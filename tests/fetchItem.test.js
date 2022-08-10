4d730f3fd3f79477d67359782e1f040ac73429c4
Guilherme Machado <machadofguilherme@gmail.com>
2022-08-10 00:44:19 -0300

    Requisito 3.

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,38 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchItem é uma função.', () => {
+    const expectative = fetchItem();
+    expect(typeof expectative).toBe('object');
+  });
+
+  it(`Executa fetchItem com o argumento do item "MLB1615760527"
+  e verifica se fetch foi chamado;`, () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toBeCalled();
+  });
+
+  it(`Verifica se ao chamar a função fetchItem com o argumento 
+  do item "MLB1615760527", a função fetch utiliza o endpoint 
+  "https://api.mercadolibre.com/items/MLB1615760527".`, () => {
+    fetchItem('MLB1615760527');
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toBeCalledWith(endpoint);
+  });
+
+  it(`Teste se o retorno da função fetchItem com o argumento do 
+  item "MLB1615760527" é uma estrutura de dados igual ao objeto 
+  item que já está importado no arquivo.`, () => {
+    const expectative = fetchItem('MLB1615760527');
+    expect(typeof expectative).toEqual(typeof item);
+  });
+
+  it(`Teste se, ao chamar a função fetchItem sem argumento, 
+  retorna um erro com a mensagem: 'You must provide an url'.`, () => {
+    try {
+      fetchItem();
+    } catch (e) {
+      return new Error('You must provide an url');
+    }
+  });
 });
